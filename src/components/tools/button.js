1d81bee257daa76139b0fa24c62f8369ae3e4b6d
Williamson911 <william.williamson.91@gmail.com>
2021-04-28 18:07:12 +0200

    props

--- src/components/tools/button.js
+++ src/components/tools/button.js
@@ -1,13 +1,14 @@
 import PropTypes from "prop-types";
 import classnames from "classnames";
 
-const Button = ({label, title, disabled=false, onClick})=>(
+const Button = ({label, title, disabled=false, onClick, ...props})=>(
     <button
         className={classnames("button", "is-priamry", "is-medium", "is-fullwidth" )}
         type={"button"}
         title={title||label}
         disabled={disabled}
         onClick={onClick}
+        {...props}
         >
             {label}
         </button>

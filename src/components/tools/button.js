12d2e85e31895b1040caaa4b033523151ca8b592
Williamson911 <william.williamson.91@gmail.com>
2021-04-28 18:01:42 +0200

    Prepare tools components

---
+++ src/components/tools/button.js
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import classnames from "classnames";
+
+const Button = ({label, title, disabled=false, onClick})=>(
+    <button
+        className={classnames("button", "is-priamry", "is-medium", "is-fullwidth" )}
+        type={"button"}
+        title={title||label}
+        disabled={disabled}
+        onClick={onClick}
+        >
+            {label}
+        </button>
+);
+
+Button.propTypes={
+    label: PropTypes.string.isRequired,
+    title: PropTypes.string,
+    disabled: PropTypes.bool,
+    onClick: PropTypes.func.isRequired,
+}
+
+export default Button;
--- src/containers/pomodoro.js
+++ src/containers/pomodoro.js
@@ -1,11 +1,27 @@
 import classNames from "classnames";
 import Display from "../components/display";
+import Tools from "../components/tools";
+
 const Pomodoro = ()=>{
     const seconds = 1500;
+    const running =false;
+
+    const handleMinus=()=>console.log("minus!")
+    const handleReset=()=>console.log("reset!")
+    const handlePlayPause=()=>console.log("play/pause!")
+    const handlePlus=()=>console.log("plus!")
     return(
         <div className={classNames("columns", "is-mobile", "is-centered")}>
             <div className={classNames("column", "is-half")}>
                 <Display seconds={seconds} />
+                <Tools
+                    running={running}
+                    onMinus={handleMinus}
+                    onReset={handleReset}
+                    onPlayPause={handlePlayPause}
+                    onPlus={handlePlus}
+                />
+
             </div>
         </div>
     );

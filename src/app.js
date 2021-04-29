a6e307fc8b512a3360987524b6ef1aafb7c1010b
Williamson911 <william.williamson.91@gmail.com>
2021-04-29 14:10:17 +0200

    fix render

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom";
 import Pomodoro from "./containers/pomodoro";
+
 import "bulma";
 import "./styles/main.scss";
+
 ReactDOM.render(<Pomodoro />, document.querySelector("#app"));
